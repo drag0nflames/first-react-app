30b826fe249794d9761120efd2c656dfb92ab0f8
Bikky Bhujel <bicky.bhujel@gmail.com>
2019-05-02 21:18:44 +0545

    Updates

---
+++ precache-manifest.bcc9e81603e35d708fb056b25ba21e00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71e29772979359d0a21b",
+    "url": "/first-react-app/static/js/runtime~main.d962f3f5.js"
+  },
+  {
+    "revision": "5d93cf9b7bb24cf1490a",
+    "url": "/first-react-app/static/js/main.224e3320.chunk.js"
+  },
+  {
+    "revision": "f6422e6e88cfb1ba2ef6",
+    "url": "/first-react-app/static/js/2.372fd93a.chunk.js"
+  },
+  {
+    "revision": "5d93cf9b7bb24cf1490a",
+    "url": "/first-react-app/static/css/main.e88a5b24.chunk.css"
+  },
+  {
+    "revision": "d6a510bb06cae5b960abf55ee592a4a1",
+    "url": "/first-react-app/index.html"
+  }
+];
